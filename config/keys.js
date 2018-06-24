4f5a4946ea35a4c90a0425e79aa7e7460e1a8fa1
Manuel Hoss <manuel.hoss@hotmail.de>
2018-06-24 19:41:00 +0200

    Authorization implemented serverside

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,6 @@
 module.exports = {
   mongoURI:
-    "mongodb://manuelhoss:manuelhoss2018@ds261660.mlab.com:61660/mernstackdevdb"
+    //"mongodb://manuelhoss:manuelhoss2018@ds261540.mlab.com:61540/mernsackdevdb"
+    "mongodb://localhost:27017/mernstackdevdb",
+  secretOrKey: "secret"
 };
